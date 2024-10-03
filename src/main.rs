198aae4120b1d148a5f54fca98a4882e7528eeb9
Oleksandr Yampolskyi <les.yampolskyi@gmail.com>
2024-10-03 23:12:18 +0300

    Write to a txt file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::fs::OpenOptions;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    let mut f = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .append(true)
+        .open("data/data.txt")
+        .unwrap();
+
+    if let Err(e) = writeln!(f, "A new line!") {
+        eprintln!("Couldn't write to file: {}", e);
+    }
 }
