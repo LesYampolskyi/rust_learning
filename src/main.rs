154ee654d89268e25728aa4100de21b4953c0e1a
Oleksandr Yampolskyi <les.yampolskyi@gmail.com>
2024-10-04 19:12:56 +0300

    Input/Output commands

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,29 @@
-use std::fs::OpenOptions;
-use std::io::Write;
-
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 fn main() {
-    let mut f = OpenOptions::new()
-        .write(true)
-        .create(true)
-        .append(true)
-        .open("data/data.txt")
-        .unwrap();
+    println!("Games: ");
 
-    if let Err(e) = writeln!(f, "A new line!") {
-        eprintln!("Couldn't write to file: {}", e);
+    let secret_number = rand::thread_rng().gen_range(1..=10);
+    println!("The secret number is: {}", secret_number);
+    loop {
+        let mut line = String::new();
+        println!("Input your guess: ");
+        io::stdin().read_line(&mut line).expect("Fail");
+        print!("Your inputs is: {}", line);
+
+        let line: u32 = match line.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match line.cmp(&secret_number) {
+            Ordering::Greater => println!("Too big"),
+            Ordering::Less => println!("Too small"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
