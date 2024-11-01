ec259f640c432c99745a4a8631610ff5a60f52d6
Oleksandr Yampolskyi <les.yampolskyi@gmail.com>
2024-11-01 20:45:29 +0200

    Cli commands, clap

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,71 @@
+use clap::{command, Arg, Command};
 use std::fs;
-fn main() {
-    println!("Program starting");
-    let directory_name = String::from("custom_data_2");
-    let read_file_path = String::from("file01.txt");
-    let text = "Test data";
-    
-    let mut path_file = String::from("./");
 
-    path_file.push_str(&directory_name);
-    path_file.push_str("/");
-    path_file.push_str(&read_file_path);
-    println!("Path is: {}", path_file);
-   
-    create_directory(&directory_name);
-    create_file(&path_file, text);
-    read_some_file(&path_file);
+fn main() {
+    let read_action = "read_file";
+    let create_action = "create";
+    let match_result = command!()
+        .subcommand(
+            Command::new(&read_action).arg(
+                Arg::new("file_name")
+                    .long("file-name")
+                    .required(true)
+                    .help("File name"),
+            ),
+        )
+        .subcommand(
+            Command::new(create_action)
+                .arg(Arg::new("dir").long("dir").help("Directory name"))
+                .arg(Arg::new("file").long("file").help("file name")),
+        )
+        .get_matches();
+
+    let action = match_result.subcommand_name();
+    println!("is read::: {}", action.unwrap());
+    if action.unwrap() == read_action {
+        println!("READ ACTION");
+        let read_args = match_result.subcommand_matches(&read_action);
+        match read_args {
+            Some(val) => {
+                let a = val.get_one::<String>("file_name").unwrap();
+                let my_path = std::path::Path::new(&a);
+                if my_path.exists() {
+                    read_file(a);
+                    return;
+                } else {
+                    println!("Fle not exist");
+                }
+            }
+            None => println!("No data????"),
+        }
+    } else if action.unwrap() == create_action {
+        let create_args = match_result.subcommand_matches(&create_action);
+        match create_args {
+            Some(val) => {
+                let dir_res = val.get_one::<String>("dir");
+                match dir_res {
+                    Some(val) => {
+                        create_directory(val);
+                        return;
+                    }
+                    None => println!("not dir"),
+                }
+
+                let file_res = val.get_one::<String>("file");
+
+                match file_res {
+                    Some(path) => {
+                        create_file(path, "Test again");
+                    }
+                    None => println!("not file"),
+                }
+            }
+            None => println!("No data????"),
+        }
+    }
 }
 
-pub fn read_some_file(path: &String) {
+pub fn read_file(path: &String) {
     println!("Here data::: {}", path);
     let convert_bytes_to_string = |mut a: String, v: &u8| {
         let new_char = char::from(*v);
