c67bb6b195e158b4559f72d2cb1c5beca4b1261c
Oleksandr Yampolskyi <les.yampolskyi@gmail.com>
2024-11-01 14:04:00 +0200

    Std. Create dir,file, write file

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,66 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use std::fs;
+fn main() {
+    println!("Program starting");
+    let directory_name = String::from("custom_data_2");
+    let read_file_path = String::from("file01.txt");
+    let text = "Test data";
+    
+    let mut path_file = String::from("./");
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+    path_file.push_str(&directory_name);
+    path_file.push_str("/");
+    path_file.push_str(&read_file_path);
+    println!("Path is: {}", path_file);
+   
+    create_directory(&directory_name);
+    create_file(&path_file, text);
+    read_some_file(&path_file);
 }
 
-#[get("/custom")]
-async fn get_data() -> impl Responder {
-    HttpResponse::Ok().body("And this is custom")
+pub fn read_some_file(path: &String) {
+    println!("Here data::: {}", path);
+    let convert_bytes_to_string = |mut a: String, v: &u8| {
+        let new_char = char::from(*v);
+        a.push(new_char);
+        a
+    };
+
+    let read_result = std::fs::read(path);
+    if read_result.is_ok() {
+        println!(
+            "From file: {}",
+            read_result
+                .ok()
+                .unwrap()
+                .iter()
+                .fold(String::from(""), convert_bytes_to_string)
+        );
+    } else {
+        println!("Some problem to read file.");
+    }
 }
 
-#[get("/custom")]
-async fn custom_request() -> impl Responder {
-    HttpResponse::Ok().body("Hello, my friend!")
+pub fn create_file(path: &String, text: &str) {
+    let my_path = std::path::Path::new(path);
+    if my_path.exists() {
+        println!("=== File already exists");
+        return;
+    }
+    println!("Create file, path: {}", path);
+    _ = std::fs::write(path, text);
 }
 
-#[post("/echo")] // POST _ NOT GET
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
-
-async fn another_request() -> impl Responder {
-    HttpResponse::Ok().body("123- hello --- ")
-}
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .service(get_data)
-            .service(custom_request)
-            .route("/hey", web::get().to(manual_hello))
-            // .route("/hi", web::get().to(custom_request))
-            .route("/hi", web::get().to(another_request))
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+pub fn create_directory(path: &String) {
+    let my_path = std::path::Path::new(path);
+    if my_path.exists() {
+        println!("Directory already exists! Skipping creating");
+        return;
+    }
+    println!("========");
+    let create_dir_result = fs::create_dir(path);
+    if create_dir_result.is_ok() {
+        println!("Directory created");
+    } else {
+        println!("Some problems {:?} ", create_dir_result.err())
+    }
 }
