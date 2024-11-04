cebc5267ee725b3f31fa2452e7df7cc494222428
Oleksandr Yampolskyi <les.yampolskyi@gmail.com>
2024-11-04 09:34:02 +0200

    Read csv file

--- src/main.rs
+++ src/main.rs
@@ -1,114 +1,46 @@
-use clap::{command, Arg, Command};
-use std::fs;
+use csv::{self, Reader};
+use std::{path::Path, result};
 
 fn main() {
-    let read_action = "read_file";
-    let create_action = "create";
-    let match_result = command!()
-        .subcommand(
-            Command::new(&read_action).arg(
-                Arg::new("file_name")
-                    .long("file-name")
-                    .required(true)
-                    .help("File name"),
-            ),
-        )
-        .subcommand(
-            Command::new(create_action)
-                .arg(Arg::new("dir").long("dir").help("Directory name"))
-                .arg(Arg::new("file").long("file").help("file name")),
-        )
-        .get_matches();
+    let file_name = "file_test.csv";
+    let result = Reader::from_path(file_name);
 
-    let action = match_result.subcommand_name();
-    println!("is read::: {}", action.unwrap());
-    if action.unwrap() == read_action {
-        println!("READ ACTION");
-        let read_args = match_result.subcommand_matches(&read_action);
-        match read_args {
-            Some(val) => {
-                let a = val.get_one::<String>("file_name").unwrap();
-                let my_path = std::path::Path::new(&a);
-                if my_path.exists() {
-                    read_file(a);
-                    return;
-                } else {
-                    println!("Fle not exist");
-                }
-            }
-            None => println!("No data????"),
+    if result.is_err() {
+        println!("Failed to read a csv file. Some error, lilbro");
+        std::process::exit(9);
+    }
+
+    println!("After error handling");
+
+    let mut my_reader =  result.unwrap();
+
+    for record in my_reader.records() {
+        println!("====++++: {:?}", record);
+        println!("====++++: {}", record.unwrap().get(0).unwrap());
+    }
+
+
+    // println!("Here we are!");
+
+    // if let Err(e) = read_csv(String::from("file_test.csv")) {
+    //     eprintln!("Error, lilbro: {}", e);
+    // };
+}
+
+fn read_csv(filename: String) -> Result<(), csv::Error> {
+    let path = Path::new(&filename);
+
+    if path.exists() {
+        println!("From read_csv() function...");
+        let mut rcsv = csv::Reader::from_path(path)?;
+
+        for result in rcsv.records() {
+            let record = result?;
+            println!("Result ===> {:?} ", record)
         }
-    } else if action.unwrap() == create_action {
-        let create_args = match_result.subcommand_matches(&create_action);
-        match create_args {
-            Some(val) => {
-                let dir_res = val.get_one::<String>("dir");
-                match dir_res {
-                    Some(val) => {
-                        create_directory(val);
-                        return;
-                    }
-                    None => println!("not dir"),
-                }
-
-                let file_res = val.get_one::<String>("file");
-
-                match file_res {
-                    Some(path) => {
-                        create_file(path, "Test again");
-                    }
-                    None => println!("not file"),
-                }
-            }
-            None => println!("No data????"),
-        }
-    }
-}
-
-pub fn read_file(path: &String) {
-    println!("Here data::: {}", path);
-    let convert_bytes_to_string = |mut a: String, v: &u8| {
-        let new_char = char::from(*v);
-        a.push(new_char);
-        a
-    };
-
-    let read_result = std::fs::read(path);
-    if read_result.is_ok() {
-        println!(
-            "From file: {}",
-            read_result
-                .ok()
-                .unwrap()
-                .iter()
-                .fold(String::from(""), convert_bytes_to_string)
-        );
     } else {
-        println!("Some problem to read file.");
+        println!("File not exists");
     }
-}
 
-pub fn create_file(path: &String, text: &str) {
-    let my_path = std::path::Path::new(path);
-    if my_path.exists() {
-        println!("=== File already exists");
-        return;
-    }
-    println!("Create file, path: {}", path);
-    _ = std::fs::write(path, text);
-}
-
-pub fn create_directory(path: &String) {
-    let my_path = std::path::Path::new(path);
-    if my_path.exists() {
-        println!("Directory already exists! Skipping creating");
-        return;
-    }
-    println!("========");
-    let create_dir_result = fs::create_dir(path);
-    if create_dir_result.is_ok() {
-        println!("Directory created");
-    } else {
-        println!("Some problems {:?} ", create_dir_result.err())
-    }
+    Ok(())
 }
---
+++ src/main_old.rs
@@ -0,0 +1,117 @@
+use clap::{command, Arg, Command};
+use std::fs;
+
+fn main() {
+    let read_action = "read_file";
+    let create_action = "create";
+    let match_result = command!()
+        .subcommand(
+            Command::new(&read_action).arg(
+                Arg::new("file_name")
+                    .long("file-name")
+                    .required(true)
+                    .help("File name"),
+            ),
+        )
+        .subcommand(
+            Command::new(create_action)
+                .arg(Arg::new("dir").long("dir").help("Directory name"))
+                .arg(Arg::new("file").long("file").help("file name")),
+        )
+        .get_matches();
+
+    let action = match_result.subcommand_name();
+    let action_result = action.unwrap();
+    println!("is read::: {}", action_result);
+    if action_result == read_action {
+        println!("READ ACTION");
+        let read_args = match_result.subcommand_matches(&read_action);
+        match read_args {
+            Some(val) => {
+                let a = val.get_one::<String>("file_name").unwrap();
+                let my_path = std::path::Path::new(&a);
+                if my_path.exists() {
+                    read_file(a);
+                    return;
+                } else {
+                    println!("Fle not exist");
+                }
+            }
+            None => println!("No data????"),
+        }
+    } else if action_result == create_action {
+        let create_args = match_result.subcommand_matches(&create_action);
+        match create_args {
+            Some(val) => {
+                let dir_res = val.get_one::<String>("dir");
+                match dir_res {
+                    Some(val) => {
+                        create_directory(val);
+                        return;
+                    }
+                    None => println!("not dir"),
+                }
+
+                let file_res = val.get_one::<String>("file");
+
+                match file_res {
+                    Some(path) => {
+                        create_file(path, "Test again");
+                    }
+                    None => println!("not file"),
+                }
+            }
+            None => println!("No data????"),
+        }
+    } else {
+        println!("No args!");
+    }
+}
+
+pub fn read_file(path: &String) {
+    println!("Here data::: {}", path);
+    let convert_bytes_to_string = |mut a: String, v: &u8| {
+        let new_char = char::from(*v);
+        a.push(new_char);
+        a
+    };
+
+    let read_result = std::fs::read(path);
+    if read_result.is_ok() {
+        println!(
+            "From file: {}",
+            read_result
+                .ok()
+                .unwrap()
+                .iter()
+                .fold(String::from(""), convert_bytes_to_string)
+        );
+    } else {
+        println!("Some problem to read file.");
+    }
+}
+
+pub fn create_file(path: &String, text: &str) {
+    let my_path = std::path::Path::new(path);
+    if my_path.exists() {
+        println!("=== File already exists");
+        return;
+    }
+    println!("Create file, path: {}", path);
+    _ = std::fs::write(path, text);
+}
+
+pub fn create_directory(path: &String) {
+    let my_path = std::path::Path::new(path);
+    if my_path.exists() {
+        println!("Directory already exists! Skipping creating");
+        return;
+    }
+    println!("========");
+    let create_dir_result = fs::create_dir(path);
+    if create_dir_result.is_ok() {
+        println!("Directory created");
+    } else {
+        println!("Some problems {:?} ", create_dir_result.err())
+    }
+}
