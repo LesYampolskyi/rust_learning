113e378e7e46bd3cc41b1732507fed7ce418bbaa
Oleksandr Yampolskyi <les.yampolskyi@gmail.com>
2024-12-11 21:22:01 +0200

    Create the smallest exe file with lib-c

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,16 @@
-use itertools::Itertools;
+#![no_std]
+#![no_main]
 
-fn main() {
-    println!("Hello world");
-    let values: Vec<u8> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    let result = values
-        .windows(4)
-        .find(|window| window == &[1, 2, 3, 4])
-        .is_some();
-
-    // dbg!(result)
-    println!("Result:: {}", result);
-    println!("Done");
-
-
-    let i = vec![1,2,3,4,5,6,7,8,9,10,11,12,13];
-    let i_slice = &i[..];
-    let i_result = i_slice.has_prefix_custom(&[9,10,11]);
-    println!("has_prefix_custom:: {}", i_result);
-    println!("+++");
-
-    let s = vec!["Hello", "World", "some", "Stuff"];
-    let s_result = s.as_slice().has_prefix_custom(&["World", "some"]);
-    println!("string has_prefix_custom:: {}", s_result);
-    println!("+++");
-}
-
-trait Prefix<T> {
-    fn has_prefix_custom(&self, prefix: &[T]) -> bool;
-}
-
-impl<T> Prefix<T> for &[T]
-where
-    T: PartialEq,
-{
-    fn has_prefix_custom(&self, prefix: &[T]) -> bool {
-        self.iter()
-            // .positions(predicate)
-            // .pos
-            .positions(|v| *v == prefix[0])
-            .find(|&index| {
-                let range = index..(index + prefix.len());
-                self[range] == *prefix
-            })
-            .is_some()
+#[no_mangle]
+pub extern "C" fn main(_argc: isize, _argv: *const *const u8) -> isize {
+    const HELLO: &'static str = "Hello, world!\n\0";
+    unsafe {
+        libc::printf(HELLO.as_ptr() as *const _);
     }
+    0
+}
+
+#[panic_handler]
+fn my_panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
 }
